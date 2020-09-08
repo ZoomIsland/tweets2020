d55400646899fe05c34ad5437ff4eea5c95105b4
ZoomIsland <zacherymilan@gmail.com>
2020-09-08 14:18:43 -0600

    Early components added

--- hackathon2020/src/App.js
+++ hackathon2020/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
 
+import SimpleMap from './containers/Map/Map'
+
 function App() {
   return (
     <div className="App">
       This is a test?
+      {/* Link to register */}
+
+      {/* Map of polling places */}
+      <SimpleMap />
+
+      {/* Link to calendar */}
+
     </div>
   );
 }
