9860028c08d858000dd3401949d444ead7d13c03
ZoomIsland <zacherymilan@gmail.com>
2020-09-09 18:59:17 -0600

    Updated questions file

--- hackathon2020/src/components/Question/Question.js
+++ hackathon2020/src/components/Question/Question.js
@@ -4,34 +4,29 @@ import questions from '../../Questions'
 import './Question.css'
 
 function Question(props) {
-  const answers = ['Joe Biden', 'Donald Trump'];
-
   const getImage = (name) => {
     switch(name) {
       case "Donald Trump":
         return <img className="answerImg" src="/Candidate_Trump.svg" alt="Donald Trump" />
         break;
       case "Joe Biden":
-        return <img className="answerImg" src="/Candidate_Trump.svg" alt="Joe Biden" />
+        return <img className="answerImg" src="/Candidate_Biden.svg" alt="Joe Biden" />
         break;
       case "Jo Jorgensen":
-        return <img className="answerImg" src="/Candidate_Trump.svg" alt="Jo Jorgensen" />
+        return <img className="answerImg" src="/Candidate_Jorgensen.svg" alt="Jo Jorgensen" />
         break;
       case "Howie Hawkins":
-        return <img className="answerImg" src="/Candidate_Trump.svg" alt="Howie Hawkins" />
+        return <img className="answerImg" src="/Candidate_Hawkins.svg" alt="Howie Hawkins" />
         break;
     }
-    //run a switch/case on the name
-    //return the image based on name
   }
 
   const tweet = questions[props.index]
-  const buttons = answers.map(answer => {
+  const buttons = tweet.answers.map(answer => {
     if(tweet.answer === answer){
       return (
       <div className="answerBtn">
         {getImage(answer)}
-        {/* <img className="answerImg" src="https://www.orbistechnologies.com/wp-content/uploads/2018/12/profile-placeholder-image-gray-silhouette-no-vector-21542863.jpg" /> */}
         <button className={props.trueBtn} key='0' disabled={props.buttonDisable} value="true" onClick={props.onAnswer}>
           {answer}
         </button>
@@ -41,7 +36,6 @@ function Question(props) {
       return (
         <div className="answerBtn">
         {getImage(answer)}
-        {/* <img className="answerImg" src="https://www.orbistechnologies.com/wp-content/uploads/2018/12/profile-placeholder-image-gray-silhouette-no-vector-21542863.jpg" /> */}
         <button className={props.falseBtn} key='1' disabled={props.buttonDisable} value="false" onClick={props.onAnswer}>
           {answer}
         </button>
